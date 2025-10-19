163f49cf6751a5a667ef5ead475102808a56f593
alibekorakbaev <orakbaevalibek3@gmail.com>
2025-10-19 18:56:14 +0500

    set up

---
+++ src/pages/admin/Dashboard.tsx
@@ -0,0 +1,9 @@
+import {Box} from "@chakra-ui/react";
+
+function Dashboard() {
+  return (
+    <Box>Dashboard</Box>
+  )
+}
+
+export default Dashboard;
---
+++ src/pages/user/Home.tsx
@@ -0,0 +1,10 @@
+import {Box} from "@chakra-ui/react";
+
+function Home(){
+  return (
+    <Box>
+      hello home page
+    </Box>
+  )
+}
+export default Home;
