d4bf113f7c038016415506e4c403b0e0be047240
alibekorakbaev <orakbaevalibek3@gmail.com>
2025-10-20 23:55:30 +0500

    login  ui

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,30 +1,102 @@
+import {Box, Container, VStack, Heading, Stack, Field, Input, Button} from "@chakra-ui/react";
+import {useForm} from "react-hook-form";
+import {useState} from "react";
 
-import {useForm, type SubmitHandler} from 'react-hook-form'
-import {Box, Input} from "@chakra-ui/react";
-
-type Inputs = {
-  login: string
-  password: string
+interface LoginProps {
+  login: string,
+  password: string,
 }
 
 function Login() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: {errors},
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-  console.log(watch("login"))
-  console.log()
+  const [showPassword, setShowPassword] = useState(false);
+  const {handleSubmit, register, formState: {errors, isSubmitting}} = useForm<LoginProps>()
+  const onSubmit = (e: LoginProps) => {
+    console.log(e)
+  }
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input placeholder={'Login'} {...register('login')}{...{required: true}}/>
-      {errors.login && <span>This field is required</span>}
-      <Input placeholder={'Password'} {...register('password')} {...{required: true}}  />
-      {errors.password && <Box as={'span'}>This field is required</Box>}
-      <input type="submit"/>
-    </form>
+    <Box minH={"100vh"} bg={"white"} direction={"flex"} alignItems={"center"} alignContent={'center'}
+         justifyContent={"center"}>
+      <Container maxW={'lg'} py={15}>
+        <Box p={8} borderRadius={'xl'} boxShadow={'md'}>
+          <VStack gap={6} align={'stretch'}>
+            <VStack gap={2}>
+              <Heading size="xl" color={'black'} textAlign="center">
+                Sign In
+              </Heading>
+
+            </VStack>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack gap={4}>
+                <Field.Root invalid={!!errors.login}>
+                  <Field.Label color={'black'} fontWeight={'medium'}>Email</Field.Label>
+                  <Input type={'text'}
+                         placeholder="login"
+                         size="lg"
+                         color="black"
+                         {...register('login', {
+                           required: "required",
+                         })}
+                  />
+
+                </Field.Root>
+
+                <Field.Root invalid={!!errors.password}>
+                  <Field.Label color="gray.700" fontWeight="medium">
+                    Parol
+                  </Field.Label>
+                  <Box w={'100%'} position="relative">
+                    <Input
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder="Parolingizni kiriting"
+                      size="lg"
+                      w={'100%'}
+                      pr="12"
+                      color="black"
+                      {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                          value: 6,
+                          message: 'Password must be at least 6 characters long',
+                        },
+                      })}
+                    />
+                    <Button
+                      position="absolute"
+                      right="2"
+                      top="50%"
+                      bg={'transparent'}
+                      transform="translateY(-50%)"
+                      size="sm"
+                      variant="ghost"
+                      onClick={() => setShowPassword(!showPassword)}
+                      type="button"
+                    >
+                      {showPassword ? '👁️' : '👁️‍🗨️'}
+                    </Button>
+                  </Box>
+
+                </Field.Root>
+
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  size="lg"
+                  fontSize="md"
+                  loading={isSubmitting}
+                  w="full"
+                  mt={4}
+                >
+                  Kirish
+                </Button>
+              </Stack>
+
+            </form>
+
+          </VStack>
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
