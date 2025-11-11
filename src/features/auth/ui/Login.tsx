a6a2c73a70aead49e083698fd3353542904301d0
user <orakbaevalibek3@gmail.com>
2025-11-11 14:25:30 +0500

    token success

--- src/features/auth/ui/Login.tsx
+++ src/features/auth/ui/Login.tsx
@@ -2,13 +2,21 @@ import {useForm} from "react-hook-form";
 import type {IUser} from "@/features/auth/type/authType.ts";
 import {AuthUi} from "@/features/auth/ui/AuthUi.tsx";
 import {useLogin} from "@/features/auth/query/authQuery.ts";
+import {useNavigate} from "react-router";
 
 function Login() {
+    const navigate = useNavigate();
   const mutation = useLogin()
   const {handleSubmit, register, formState: {errors, isSubmitting}} = useForm<IUser>()
   const onSubmit = (e: IUser) => {
-    mutation.mutate(e)
-
+    mutation.mutate(e,{
+        onSuccess: () => {
+            navigate('/')
+        },
+        onError: () => {
+            alert('Login and password error')
+        }
+    })
   }
 
 
--- src/pages/admin/sideBar/SideBar.tsx
+++ src/pages/admin/sideBar/SideBar.tsx
@@ -7,14 +7,14 @@ export const SideBar = () => {
       <Box>
         <Box>
           <Box w={'270px'} height={'64px'} display="flex" flexDirection="row" borderBottom={'1px solid'}
-               padding={' 25px'} borderColor={'#404040'} alignContent={'flex-start'} gap={4} alignItems="center">
+               padding={'25px'} borderColor={'#404040'} alignContent={'flex-start'} gap={4} alignItems="center">
 
             <Box>
               <Image width={'60px'} height={'60px'} src={logo} alt="Softium"/>
             </Box>
 
             <Box>
-              <Text textStyle={'xl'}>Softium</Text>
+              <Text textStyle={'xl'} >Softium</Text>
             </Box>
 
           </Box>
